099803604494412bb4ed2ebd9be0a44a8d72ce02
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-03-08 19:46:25 +0300

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/offers" element={<Offers/>}/>
+          <Route path="/forgot-password" element={<ForgotPassword/>}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="/sign-in" element={<Signin/>}/>
+        </Routes>
+      </Router>
     </>
   );
 }
